818a293f05a71844d6eb8e918011a638a1d968a6
Riccardo Binetti <rbino@gmx.com>
2023-01-09 15:24:25 +0100

    Add conditional compilation for optional dependencies
    The package implements three different parts, depending respectively on Ecto,
    Phoenix.HTML and Plug. Those 3 dependencies were listed as optional, but the
    compilation actually failed (or printed some warnings) if some of them were
    missing.
    
    This commit adds conditional compilation by wrapping modules belonging to the
    three parts in
    
    ```elixir
    if Code.ensure_loaded?(x) do
      ...
    end
    ```
    
    Where `x` is `Ecto`/`Phoenix.HTML`/`Plug`.

--- lib/ecto/translatable_type.ex
+++ lib/ecto/translatable_type.ex
@@ -1,32 +1,34 @@
-defmodule I18nHelpers.Ecto.TranslatableType do
-  use Ecto.Type
+if Code.ensure_loaded?(Ecto) do
+  defmodule I18nHelpers.Ecto.TranslatableType do
+    use Ecto.Type
 
-  # data type we want to use to store our custom value at the database level
-  def type, do: :map
+    # data type we want to use to store our custom value at the database level
+    def type, do: :map
 
-  # takes a value from an external source (for example a user input) and
-  # converts it into a format that Ecto can work with
-  def cast(translations) when translations == %{}, do: {:ok, nil}
+    # takes a value from an external source (for example a user input) and
+    # converts it into a format that Ecto can work with
+    def cast(translations) when translations == %{}, do: {:ok, nil}
 
-  def cast(%{} = translations) do
-    translations_without_empty =
-      translations
-      |> Enum.reject(fn {_, v} -> String.trim(v) == "" end)
-      |> Map.new()
-      |> (fn
-            map when map == %{} -> nil
-            map -> map
-          end).()
+    def cast(%{} = translations) do
+      translations_without_empty =
+        translations
+        |> Enum.reject(fn {_, v} -> String.trim(v) == "" end)
+        |> Map.new()
+        |> (fn
+              map when map == %{} -> nil
+              map -> map
+            end).()
 
-    {:ok, translations_without_empty}
+      {:ok, translations_without_empty}
+    end
+
+    def cast(nil), do: {:ok, nil}
+    def cast(_), do: :error
+
+    # converts the raw value pulled from the database into an Elixir value
+    def load(term), do: Ecto.Type.load(:map, term)
+
+    # takes an Elixir value and converts it into a value that the database recognizes
+    def dump(term), do: Ecto.Type.dump(:map, term)
   end
-
-  def cast(nil), do: {:ok, nil}
-  def cast(_), do: :error
-
-  # converts the raw value pulled from the database into an Elixir value
-  def load(term), do: Ecto.Type.load(:map, term)
-
-  # takes an Elixir value and converts it into a value that the database recognizes
-  def dump(term), do: Ecto.Type.dump(:map, term)
 end
